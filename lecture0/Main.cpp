4048c849498ca50cd7ac141425ab67bc156da4ef
Hoshi Ran <ureshinostyle350@gmail.com>
2024-08-13 14:49:35 +0900

    feat: Point

--- lecture0/Main.cpp
+++ lecture0/Main.cpp
@@ -1,7 +1,7 @@
 ﻿#include "MyLib/Common.hpp"  // - 13:50
+#include "MyLib/Point.hpp"   // - 14:20
 #include "MyLib/Utility.hpp" // - 14:00
 #include <print>
-// #include "MyLib/Point.hpp" // - 14:20
 // #include "MyLib/FileSystem.hpp" // - 14:30
 // #include "MyLib/Timer.hpp"// - 14:40
 //  [14:40 - 14:55 break time]
@@ -36,6 +36,12 @@ int main()
 
     std::println("---- Point.hpp ----");
     {
+
+        Point p1 = {1, 2};
+        std::println("p1 = ({}, {})", p1.x, p1.y);
+        std::println("p1.length() = {}", p1.length());
+        std::println("p1.length<float>() = {}", p1.length<float>());
+        std::println("p1 = {}", p1);
     }
 
     std::println("---- FileSystem.hpp ----");
