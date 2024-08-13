112bd7c34c3f9463d8799e465ae66626717a9a1a
Hoshi Ran <ureshinostyle350@gmail.com>
2024-08-13 14:51:14 +0900

    feat: FileSystem

--- lecture0/MyLib/FileSystem.cpp
+++ lecture0/MyLib/FileSystem.cpp
@@ -1,6 +1,18 @@
 ﻿#include "FileSystem.hpp"
+#include <filesystem> // std::filesystem::path, std::filesystem::absolute
 
 namespace seccamp
 {
-
+namespace FileSystem
+{
+std::string FullPath(const std::string_view path)
+{
+    return std::filesystem::absolute(path).string();
 }
+
+std::string Extension(const std::string_view path)
+{
+    return std::filesystem::path{path}.extension().string();
+}
+} // namespace FileSystem
+} // namespace seccamp
