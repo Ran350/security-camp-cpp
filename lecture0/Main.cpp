cb5733ad367f44bb9dd75867ef6f2f08bb187219
Hoshi Ran <ureshinostyle350@gmail.com>
2024-08-13 13:45:38 +0900

    clone N2

---
+++ lecture0/Main.cpp
@@ -0,0 +1,75 @@
+﻿#include <print>
+#include "MyLib/Common.hpp" // - 13:50
+//#include "MyLib/Utility.hpp" // - 14:00
+//#include "MyLib/Point.hpp" // - 14:20
+//#include "MyLib/FileSystem.hpp" // - 14:30
+//#include "MyLib/Timer.hpp"// - 14:40
+// [14:40 - 14:55 break time]
+//#include "MyLib/BinaryFileWriter.hpp" // - 15:10
+//#include "MyLib/BinaryFileReader.hpp" // - 15:25
+//#include "MyLib/TextFileWriter.hpp" // - 15:40
+//#include "MyLib/TextFileReader.hpp" // - 15:55
+// [15:55 - 16:10 break time]
+//#include "MyLib/Color.hpp" // - 16:30
+//#include "MyLib/Image.hpp" // - 16:50
+//#include "MyLib/BMP.hpp" // - 17:10
+
+int main()
+{
+	std::println("Hello, SecCamp!");
+
+	std::println("---- Common.hpp ----");
+	{
+
+	}
+
+	std::println("---- Utility.hpp ----");
+	{
+
+	}
+
+	std::println("---- Point.hpp ----");
+	{
+
+	}
+
+	std::println("---- FileSystem.hpp ----");
+	{
+
+	}
+
+	std::println("---- Timer.hpp ----");
+	{
+
+	}
+
+	std::println("---- BinaryFileWriter.hpp ----");
+	{
+
+	}
+
+	std::println("---- BinaryFileReader.hpp ----");
+	{
+
+	}
+
+	std::println("---- TextFileWriter.hpp ----");
+	{
+
+	}
+
+	std::println("---- TextFileReader.hpp ----");
+	{
+
+	}
+
+	std::println("---- Color.hpp ----");
+	{
+
+	}
+
+	std::println("---- Image.hpp & BMP.hpp ----");
+	{
+
+	}
+}
---
+++ lecture1/README.md
@@ -0,0 +1,30 @@
+## 1. 基本ライブラリ
+
+| ライブラリ名 | 概要 |
+|:---|:---|
+| [Common](MyLib/Common.hpp) | 各ライブラリで共通してインクルードされるヘッダ |
+| [Utility](MyLib/Utility.hpp) | 便利な関数やクラス |
+| [Point](MyLib/Point.hpp) | 2 次元座標（整数）を表すクラス |
+| [FileSystem](MyLib/FileSystem.hpp) | ファイルシステム関連 |
+| [Timer](MyLib/Timer.hpp) | 経過時間の計測 |
+| [BinaryFileWriter](MyLib/BinaryFileWriter.hpp) | バイナリファイルを書き込むクラス |
+| [BinaryFileReader](MyLib/BinaryFileReader.hpp) | バイナリファイルを読み込むクラス |
+| [TextFileWriter](MyLib/TextFileWriter.hpp) | テキストファイルを書き込むクラス |
+| [TextFileReader](MyLib/TextFileReader.hpp) | テキストファイルを読み込むクラス |
+| [Color](MyLib/Color.hpp) | 色を表すクラス |
+| [Image](MyLib/Image.hpp) | 画像を表すクラス |
+| [BMP](MyLib/BMP.hpp) | BMP ファイルを読み書きする関数 |
+
+## 2. 発展ライブラリ（選択課題）
+
+| ライブラリ名 | 概要 |
+|:---|:---|
+| [Rect](MyLib/Rect.hpp) | 長方形（整数座標）を表すクラス |
+| [Vector2D](MyLib/Vector2D.hpp) | 2 次元ベクトル（浮動小数点数）を表すクラス |
+| [Unicode](MyLib/Unicode.hpp) | Unicode 文字列を扱う関数 |
+| [INIFile](MyLib/INIFile.hpp) | INI ファイルを読み書きするクラス |
+| [PNG](MyLib/PNG.hpp) | PNG ファイルを読み書きする関数 |
+| [Paint](MyLib/Paint.hpp) | 画像に図形等を描画する関数 |
+| [Wave](MyLib/Wave.hpp) | 音声波形を扱うクラス |
+| [WAV](MyLib/WAV.hpp) | WAV ファイルを読み書きする関数 |
+| [Synthesizer](MyLib/Synthesizer.hpp) | 音声合成を行う関数 |
